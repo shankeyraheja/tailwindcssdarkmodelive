6004d6bf1b928604da53395f1678d15c77127baa
shankey raheja <shankeyraheja@gmail.com>
2025-01-06 13:50:11 +0530

    a

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react"
 
 const Card = () => {
   return (
-    <div className='p-4 bg-white shadow text-gray-900 dark:bg-black dark:text-yellow-100'>
-    <h2 className='text-xl font-bold'>Dark Mode Card</h2>
-      <p className='mt-2'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    <div className="p-4 bg-white shadow text-gray-900 dark:bg-black dark:text-yellow-100">
+      <h2 className="text-xl font-bold">Dark Mode Card</h2>
+      <p className="mt-2">
+        L'orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
